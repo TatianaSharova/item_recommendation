a4ace43f8c60431961d809fabe0d035e0927b961
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-12-14 00:02:43 +0400

    add fastapi app

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from contextlib import asynccontextmanager
+
+from app.db import create_table, delete_tables
+from fastapi import FastAPI
+from app.routers import (gen_recommendation_router, item_router, purchase_router,
+                     recommendation_router, user_router)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_table()
+    print('start')
+    yield
+    # await delete_tables()
+    print('end')
+
+app = FastAPI(lifespan=lifespan)
+
+
+app.include_router(user_router)
+app.include_router(item_router)
+app.include_router(purchase_router)
+app.include_router(recommendation_router)
+app.include_router(gen_recommendation_router)
